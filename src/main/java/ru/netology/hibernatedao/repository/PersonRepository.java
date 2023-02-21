bd5b538d84a6bf0576b1455178ba1234c6d1383a
kvbwork <kvbdev@yandex.ru>
2023-02-21 19:29:31 +0500

    JPQL query methods

--- src/main/java/ru/netology/hibernatedao/repository/PersonRepository.java
+++ src/main/java/ru/netology/hibernatedao/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package ru.netology.hibernatedao.repository;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.netology.hibernatedao.entity.Person;
 import ru.netology.hibernatedao.entity.PersonKey;
 
@@ -9,12 +11,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, PersonKey> {
-    List<Person> findByCityOfLiving(String city);
+    @Query("select p from Person p where p.cityOfLiving = :city")
+    List<Person> findByCityOfLiving(@Param("city") String city);
 
-    List<Person> findByPersonKeyAgeLessThan(int age, Sort sort);
+    @Query("select p from Person p where p.personKey.age < :age")
+    List<Person> findByPersonKeyAgeLessThan(@Param("age") int age, Sort sort);
 
-    List<Person> findByPersonKeyNameAndPersonKeySurname(String name, String surname);
+    @Query("select p from Person p where p.personKey.name = :name and p.personKey.surname = :surname")
+    List<Person> findByPersonKeyNameAndPersonKeySurname(@Param("name") String name, @Param("surname") String surname);
 
-    Optional<Person> findFirstByPersonKeyNameAndPersonKeySurname(String name, String surname);
+    @Query("select p from Person p where p.personKey.name = :name and p.personKey.surname = :surname")
+    Optional<Person> findFirstByPersonKeyNameAndPersonKeySurname(@Param("name") String name, @Param("surname") String surname);
 
 }
